65c2775fdfc2dcd17c4f3e72779fbfd75e764fbc
不断同学 <buduan461@qq.com>
2025-07-27 13:47:26 +0800

    fix: 创建admin layout，加入sidebar(未完成)

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,14 +1,59 @@
-<template>
-  <div class="admin-layout">
-    <h2>管理后台</h2>
-    <router-view />
-  </div>
-</template>
-
-<script setup lang="ts">
-
+<script lang="ts">
+export const description
+  = 'A sidebar that collapses to icons.'
+export const iframeHeight = '800px'
+export const containerClass = 'w-full h-full'
 </script>
 
-<style scoped>
+<script setup lang="ts">
+import AppSidebar from '../components/AppSidebar.vue'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '../components/ui/Breadcrumb'
+import { Separator } from '../components/ui/Separator'
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '../components/ui/sidebar'
+</script>
 
-</style>
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+      <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <div class="flex items-center gap-2 px-4">
+          <SidebarTrigger class="-ml-1" />
+          <Separator orientation="vertical" class="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem class="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator class="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div class="aspect-video rounded-xl bg-muted/50" />
+          <div class="aspect-video rounded-xl bg-muted/50" />
+          <div class="aspect-video rounded-xl bg-muted/50" />
+        </div>
+        <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
+</template>
