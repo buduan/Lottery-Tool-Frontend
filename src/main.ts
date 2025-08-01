3074005143d334c0efe7d302c33067efa8baa81a
Baicen <buduan461@gmail.com>
2025-08-02 02:24:56 +0800

    feat: 添加pinia状态管理及持久化支持

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './index.css'
+import { createPinia } from 'pinia'
+import { createPersistedState } from 'pinia-plugin-persistedstate';
+import App from './App.vue';
+import router from './router';
+import './index.css';
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+pinia.use(createPersistedState());
+app.use(pinia);
+app.use(router);
+app.mount('#app');
