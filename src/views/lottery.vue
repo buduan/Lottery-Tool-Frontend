00d923575de0c5a6d660965c8b1ce77363356e5c
buduan <39254250+buduan@users.noreply.github.com>
2025-09-17 05:27:40 +0800

    feat: 添加抽奖页面及相关配置
    添加抽奖页面视图组件和类型定义
    配置eslint忽略规则和tsconfig排除项
    新增eslint配置文件

---
+++ src/views/lottery.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="w-full p-4 bg-white[0.8] backdrop-blur-2xl">
+    <div class="h-2 overflow-clip">
+      <!-- 标识 -->
+      <image
+        v-if="basicInfo.icon"
+        :src="basicInfo.icon"
+        mode="scaleToFill"
+        class="h-full"
+      />
+
+      <span v-else class="font-bold text-2xl">
+        {{ basicInfo.title }}
+      </span>
+    </div>
+  </header>
+  <!-- 抽奖码输入框 -->
+  <div>
+    <Input v-model="lotteryCode">
+      
+    </Input>
+  </div>
+  
+  <!-- 键盘 移动端显示 -->
+  <div>
+
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Input } from '@/components/ui/input';
+import { lotteryApi } from '@/api';
+import { lotteryInfo } from '@/types/lottery';
+
+const urlParams = new URLSearchParams(window.location.search);
+
+const lotteryCode = ref<string>('');
+const basicInfo = ref<lotteryInfo>({
+  id: 0,
+  icon: 'logo.png',
+  title: '抽奖',
+  description: '这是一个测试抽奖',
+  codeFormat: '4_digit_number',
+
+});
+
+const activityId = Number(urlParams.get('activityId'));
+
+
+// TODO: 基础信息查询逻辑
+const getActivityInfo = lotteryApi.getActivity(activityId);
+getActivityInfo;
+
+
+</script>
+
+<style lang="less" module>
+
+</style>
